a946fd5e1463123968281e1ccb1b5fa81d296aef
Greg Peterson <gpeterson2@gmail.com>
2015-03-01 14:24:32 -0600

    Add kanjidic2 parser to options.

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import codecs
 import os
 import sys
 
-from jmdict.parser.jmdict import Parser
+from jmdict.parser.jmdict import Parser as JmdParser
+from kanjidic2.parser import Parser as KdParser
 from jmdict.data import SqliteWriter, SqliteReader
 from jmdict.utils.observer import ConsoleViewer
 
 def main():
     parser = argparse.ArgumentParser(description='Import edict xml')
+    parser.add_argument('--type', dest='parser_type', action='store',
+        help='Type of file to import "jmdict" or "kanjidic2"')
     parser.add_argument('--list', dest='list_values', action='store_true',
         help='Display current dictionary')
     parser.add_argument('--import', '-i', dest='import_file',
@@ -23,6 +26,11 @@ def main():
     is_verbose = args.verbose
     filename = args.import_file
     list_values = args.list_values
+    parser_type = args.parser_type
+
+    if parser_type not in ('jmdict', 'kanjidic2'):
+        print('invalid type')
+        exit(1)
 
     if filename:
         if not os.path.exists(filename):
@@ -30,21 +38,29 @@ def main():
             exit(1)
 
         viewer = ConsoleViewer()
-        parser = Parser()
+        parser = None
+
+        if parser_type == 'jmdict':
+            parser = JmdParser()
+        else:
+            parser = KdParser()
+
         parser.attach(viewer)
 
-        entries = parser.parse_from_file(filename)
+        values = parser.parse_from_file(filename)
 
-        writer = SqliteWriter()
-        writer.attach(viewer)
-        writer.write(entries)
+        print(len(values))
 
-    if list_values:
-        reader = SqliteReader()
-        entries = reader.read()
+        #writer = SqliteWriter()
+        #writer.attach(viewer)
+        #writer.write(entries)
 
-        for entry in entries:
-            print(entry)
+    #if list_values:
+        #reader = SqliteReader()
+        #entries = reader.read()
+        #
+        #for entry in entries:
+            #print(entry)
 
 if __name__ == '__main__':
     main()
